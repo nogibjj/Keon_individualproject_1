b04391a93dc4f183a501de9aba47664fbbb5b15b
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-18 06:37:29 +0000

    made it switch from pandas to polars

--- src/lib.py
+++ src/lib.py
@@ -1,13 +1,17 @@
-import pandas as pd
+import polars as pl
 import matplotlib.pyplot as plt
 
-def stat_mean(df: pd.DataFrame, col: str) -> float:
+
+def stat_summary(df: pl.DataFrame, col: str):
+    return df.describe()
+
+def stat_mean(df: pl.DataFrame, col: str) -> float:
     return df[col].sum() / len(df[col])
 
-def stat_median(df: pd.DataFrame, col: str) -> float:
+def stat_median(df: pl.DataFrame, col: str) -> float:
     return df[col].median()
 
-def stat_std(df: pd.DataFrame, col: str) -> float:
+def stat_std(df: pl.DataFrame, col: str) -> float:
     return df[col].std()
 
 def visualize_data(df):
@@ -19,9 +23,10 @@ def visualize_data(df):
 
 
 if __name__ == '__main__':
-    cars = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
+    cars = pl.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
     print(cars.head())
     print(stat_mean(cars, 'mpg'))
     print(stat_median(cars, 'mpg'))
     print(stat_std(cars, 'mpg'))
+    print(stat_summary(cars, "mpg"))
     visualize_data(cars)
